407ffbb9887833a10fa25b21e6cc01893d5d7c7a
David <dawid.cedrych@gmail.com>
2017-01-21 17:55:09 +0100

    ...

--- ExpandableCellsExample/ExpandableCell.swift
+++ ExpandableCellsExample/ExpandableCell.swift
@@ -15,6 +15,11 @@ class ExpandableCell: UITableViewCell {
 
     @IBOutlet weak var imgHeightConstraint: NSLayoutConstraint!
 
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//        
+//        self.imgHeightConstraint.constant = selected ? 128.0 : 0.0
+//    }
     
     var isExpanded:Bool = false
         {
--- ExpandableCellsExample/ViewController.swift
+++ ExpandableCellsExample/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let imgs = ["0", "1", "2", "0", "1", "2", "0", "1", "2", "0", "1", "2", "0", "1", "2", "0", "1", "2", "0", "1", "2"]
+    
+    var expandedRows = Set<Int>()
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,49 +21,61 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.tableView.dataSource = self
         self.tableView.estimatedRowHeight = 2.0
         self.tableView.rowHeight = UITableViewAutomaticDimension
-        self.tableView.tableFooterView = UIView()
+//        self.tableView.tableFooterView = UIView()
     }
 
 
     // TableView DataSource methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 21
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:ExpandableCell = tableView.dequeueReusableCell(withIdentifier: "ExpandableCell") as! ExpandableCell
-        cell.img.image = UIImage(named: indexPath.row.description)
-        cell.isExpanded = false
+        cell.img.image = UIImage(named: imgs[indexPath.row])
+        cell.isExpanded = self.expandedRows.contains(indexPath.row)
         return cell
     }
     
     // TableView Delegate methods
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        print(indexPath.row)
         guard let cell = tableView.cellForRow(at: indexPath) as? ExpandableCell
             else { return }
         
-            UIView.animate(withDuration: 0.3, animations: {
+//            UIView.animate(withDuration: 0.3, animations: {
+//                let offset: CGPoint = self.tableView.contentOffset
+        
                 tableView.beginUpdates()
-                cell.isExpanded = !cell.isExpanded
-                tableView.scrollToRow(at: indexPath, at: UITableViewScrollPosition.top, animated: true)
-                tableView.endUpdates()
+//                cell.isExpanded = !cell.isExpanded
                 
-            })
-            
+                if self.expandedRows.contains(indexPath.row) {
+                    self.expandedRows.remove(indexPath.row)
+                    cell.isExpanded = false
+                } else {
+                    self.expandedRows.insert(indexPath.row)
+                    cell.isExpanded = true
+                }
+                
+//                self.tableView.layer.removeAllAnimations()
+//                self.tableView.setContentOffset(offset, animated: false)
+//                tableView.scrollToRow(at: indexPath, at: UITableViewScrollPosition.top, animated: true)
+                tableView.endUpdates()
+               
+                
+                
+//            })
+//            
         }
-    
-    
-
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         guard let cell = tableView.cellForRow(at: indexPath) as? ExpandableCell
             else { return }
-        UIView.animate(withDuration: 0.3, animations: {
-            tableView.beginUpdates()
-            cell.isExpanded = false
-            tableView.endUpdates()
-        })
+//        UIView.animate(withDuration: 0.3, animations: {
+//            tableView.beginUpdates()
+//            cell.isExpanded = false
+//            tableView.endUpdates()
+//        })
     }
 }
 
